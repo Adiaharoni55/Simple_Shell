034565edd0036f4f4452df440c0ddd0721c8b4f5
Adiaharoni55 <112858309+Adiaharoni55@users.noreply.github.com>
2024-06-09 19:07:09 +0300

    ex2 Support jobs

--- ex1.c
+++ ex1.c
@@ -1,6 +1,7 @@
 //211749361
 
 #include <stdio.h>
+#include <signal.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,19 +19,34 @@ struct NodeList{
 
 } typedef NodeList;
 
+// struct Jobs:
+struct Jobs{
+    pid_t pid;
+    int count;
+    char command[INPUT_SIZE];
+    struct Jobs *next;
+} typedef Jobs;
+
 // data struct to keep elements the functions use
 struct Data {
     NodeList* alias_lst;
-    int cmd_count,
-            alias_count,
+    int alias_count,
             script_lines_count,
             apostrophes_count;
-    bool exit_flag,
-            error_flag,
+    bool error_flag,
+            exit_flag,
             wait_flag;
     char* multiple_commands;
 } typedef Data;
 
+
+// functions for struct Jobs:
+void  delete_job(pid_t pid);
+void add_job(pid_t pid, char* command);
+void print_jobs(Jobs* jobs_head);
+void free_jobs();
+
+// functions for struct NodeLIst:
 void free_lst(NodeList* head);
 void free_node(NodeList** node);
 NodeList* push(NodeList *head, char* name, char* commend, Data* data, char*args[]);
@@ -41,19 +57,41 @@ void print_lst(NodeList* head);
 int word_to_arg(const char* input, int arg_ind, int* input_ind, char* args[], Data* data);
 int input_to_arg(const char* input, char* arg[], Data* data, int *input_ind, int *arg_ind);
 void run_script_file(const char* file_name, char* arg[], Data* data);
-int run_shell_command(char* arg[], Data* data);
+void run_shell_command(char* arg[], Data* data, int is_quotes);
 void run_input_command(const char* input, Data* data, int start_index_input);
 void free_arg(char* arg[]);
 void skip_spaces_tabs(const char* str, int *index);
-void run_arg_command(char* args[], Data* data, int apostrophes);
-#define ASSERT_MALLOC(condition) if(!(condition)) { perror("malloc"); free_lst(data->alias_lst); free_arg(args); exit(EXIT_FAILURE); }
+void run_arg_command(char* args[], Data* data, int is_quotes);
+char* arg_into_str(char* args[]);
+#define ASSERT_MALLOC(condition) if(!(condition)) { perror("malloc"); free_lst(data->alias_lst); free_jobs(); free_arg(args); exit(EXIT_FAILURE); }
 #define ASSERT_AND_FREE(condition) if (!(condition)) { data->error_flag = true; printf("ERR\n"); free_arg(args); return; }
 
+// global elements so could change by signal:
+int cmd_count = 0;
+Jobs* jobs = NULL;
+int job_count = 1;
+
+void child_error_handler(){
+    cmd_count--;
+}
+
+void handle_sigchld(int sig, siginfo_t *info, void *ucontext){
+    pid_t pid = info->si_pid;
+    delete_job(pid);
+    kill(pid, SIGKILL);
+}
+
 int main() {
+    struct sigaction sa;
+    sa.sa_sigaction = handle_sigchld;  // Use sa_sigaction instead of sa_handler
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;  // Set the SA_SIGINFO flag
+    sigaction(SIGCHLD, &sa, NULL);
+    signal(SIGUSR1, child_error_handler);
     char input[INPUT_SIZE];
-    Data data = {NULL, 0, 0, 0, 0, false, false, false, NULL};
+    Data data = {NULL,0, 0, 0, false, false, NULL};
     while(true){
-        printf("#cmd:%d|#alias:%d|#script lines:%d> ",data.cmd_count, data.alias_count, data.script_lines_count);
+        printf("#cmd:%d|#alias:%d|#script lines:%d> ",cmd_count, data.alias_count, data.script_lines_count);
         if(fgets(input, INPUT_SIZE, stdin) == NULL){
             continue;
         }
@@ -66,10 +104,17 @@ int main() {
         }
     }
     free_lst(data.alias_lst);
+    free_jobs();
     return 0;
 }
 
 void run_input_command(const char* input, Data* data, int start_index_input){
+//    Jobs *temp = jobs;
+//    while(temp != NULL){
+//        if(!is_process_running(temp->pid)){
+//            delete_job(temp->pid);
+//        }
+//    }
     data->multiple_commands = NULL;
     data->error_flag = false;
     data->wait_flag = true;
@@ -78,39 +123,44 @@ void run_input_command(const char* input, Data* data, int start_index_input){
         args[i] = NULL;
     }
     int args_ind = 0;
-    int apostrophes = input_to_arg(input, args, data, &start_index_input, &args_ind);
-    if(args_ind > 0) args_ind --;
-    if(args[args_ind] == NULL){
-        return;
-    }
+    int is_quotes = input_to_arg(input, args, data, &start_index_input, &args_ind);
+    ASSERT_AND_FREE(is_quotes >= 0);
     if(data->exit_flag){
         free_arg(args);
         return;
     }
-
-    ASSERT_AND_FREE(apostrophes >= 0);
     if(data->multiple_commands == NULL) {
         if(strcmp(args[args_ind], "&") == 0){
             free(args[args_ind]);
             args[args_ind] = NULL;
             data->wait_flag = false;
         }
-        run_arg_command(args, data, apostrophes);
+        else if(args[args_ind][strlen(args[args_ind] - 1)] == '&'){
+            args[args_ind][strlen(args[args_ind] - 1)] = '\0';
+            data->wait_flag = false;
+        }
+        run_arg_command(args, data, is_quotes);
         return;
     }
     free(args[args_ind]);
-    args[args_ind] = NULL;
-    if(args_ind > 0) args_ind --;
+    args[args_ind--] = NULL;
+
     if(args[args_ind] != NULL){
         if(strcmp(args[args_ind], "&") == 0){
             free(args[args_ind]);
             args[args_ind] = NULL;
             data->wait_flag = false;
         }
-        run_arg_command(args, data, apostrophes);
+        else if(args[args_ind][strlen(args[args_ind] - 1)] == '&'){
+            args[args_ind][strlen(args[args_ind] - 1)] = '\0';
+            data->wait_flag = false;
+        }
+        run_arg_command(args, data, is_quotes);
     }
     free_arg(args);
-    if((strcmp(data->multiple_commands, "&&") == 0 && data->error_flag) || (strcmp(data->multiple_commands, "||") == 0 && !(data->error_flag)) || input[start_index_input] == '\0'){
+    if((strcmp(data->multiple_commands, "&&") == 0 && data->error_flag)
+            || (strcmp(data->multiple_commands, "||") == 0 && !(data->error_flag))
+            || input[start_index_input] == '\0'){
         free(data->multiple_commands);
         data->multiple_commands = NULL;
         return;
@@ -125,11 +175,11 @@ void run_input_command(const char* input, Data* data, int start_index_input){
  * 2. checks if it's alias/unalias/shell command and runs it accordingly.
  * @param input command from user / line from file.
  */
-void run_arg_command(char* args[], Data* data, int apostrophes){
+void run_arg_command(char* args[], Data* data, int is_quotes){
     if(strcmp(args[0], "alias") == 0){
         if(args[1] == NULL){
             print_lst(data->alias_lst);
-            data->cmd_count++;
+            cmd_count++;
             free_arg(args);
             return;
         }
@@ -140,15 +190,15 @@ void run_arg_command(char* args[], Data* data, int apostrophes){
             ASSERT_AND_FREE(strlen(args[1]) != 0)
             data->alias_lst = push(data->alias_lst ,args[1], args[2], data, args);
             free_arg(args);
-            if(apostrophes > 0) data->apostrophes_count++;
-            data->cmd_count++;
+            if(is_quotes > 0) data->apostrophes_count++;
+            cmd_count++;
             return;
         }
         ASSERT_AND_FREE(strcmp(args[2], "=") == 0 && args[3] != NULL && args[4] == NULL)
         data->alias_lst = push(data->alias_lst, args[1], args[3], data, args);
         free_arg(args);
-        if(apostrophes > 0) data->apostrophes_count++;
-        data->cmd_count++;
+        if(is_quotes > 0) data->apostrophes_count++;
+        cmd_count++;
         return;
     }
     if(strcmp(args[0], "unalias") == 0){
@@ -156,8 +206,8 @@ void run_arg_command(char* args[], Data* data, int apostrophes){
         data->alias_lst = delete_node(data->alias_lst, args[1], data);
         free_arg(args);
         if(!data->error_flag){
-            if(apostrophes > 0) data->apostrophes_count++;
-            data->cmd_count++;
+            if(is_quotes > 0) data->apostrophes_count++;
+            cmd_count++;
             return;
         }
         printf("ERR\n");
@@ -170,9 +220,15 @@ void run_arg_command(char* args[], Data* data, int apostrophes){
         run_script_file(args[1], args, data);
         free_arg(args);
         if(!data->error_flag) {
-            if(apostrophes > 0) data->apostrophes_count++;
-            data->cmd_count++;
+            if(is_quotes > 0) data->apostrophes_count++;
+            cmd_count++;
         }
+        return;
+    }
+    if(strcmp(args[0], "jobs") == 0){
+        ASSERT_AND_FREE(args[1] == NULL)
+        print_jobs(jobs);
+        cmd_count++;
         return;
     }
     if(strcmp(args[0], "cd") == 0){
@@ -182,18 +238,11 @@ void run_arg_command(char* args[], Data* data, int apostrophes){
             free_arg(args);
             return;
         }
-        data->cmd_count++;
+        cmd_count++;
         free_arg(args);
         return;
     }
-    int status = run_shell_command(args, data);
-    if (status == 0) {
-        data->cmd_count++;
-        if(apostrophes > 0) data->apostrophes_count++;
-    }
-    else{
-        data->error_flag = true;
-    }
+    run_shell_command(args, data, is_quotes);
     free_arg(args);
 }
 
@@ -331,6 +380,7 @@ int input_to_arg(const char input[], char* args[], Data* data, int* input_ind, i
         }
         apostrophe_update += AU;
     }
+    (*arg_ind)--;
     return apostrophe_update;
 }
 
@@ -366,33 +416,50 @@ void run_script_file(const char* file_name, char* args[], Data* data){
  * uses fork and args to run the command.
  * @return status: 0 for success, otherwise - error.
  */
-int run_shell_command(char* args[], Data *data){
+void run_shell_command(char* args[], Data *data, int is_quotes){
     pid_t pid = fork();
     if (pid < 0) {
         perror("fork");
         free_arg(args);
         free_lst(data->alias_lst);
+        free_jobs();
         exit(EXIT_FAILURE);
     } else if (pid > 0) { // parent
-        int status;
         if(data->wait_flag) {
+            pause();
+            int status;
             waitpid(pid, &status, 0);
             if (WIFEXITED(status)) {
-                free_arg(args);
-                return WEXITSTATUS(status);
+                if (WEXITSTATUS(status) == 0){
+                    cmd_count++;
+                    if(is_quotes > 0){
+                        data->apostrophes_count++;
+                    }
+                    return;
+                }
+                else{
+                    data->error_flag = true;
+                }
             } else {
                 // Child process did not terminate normally
-                free_arg(args);
-                return ERROR_VALUE;
+                data->error_flag = true;
             }
         }
+        else{
+            add_job(pid, arg_into_str(args));
+            cmd_count++;
+            usleep(100000);
+        }
+        free_arg(args);
     } else { // child
         execvp(args[0], args);
         perror("exec");
         free_arg(args);
+        if(!(data->wait_flag)) {
+            kill(getppid(), SIGUSR1);
+        }
         _exit(EXIT_FAILURE);
     }
-    return 0;
 }
 
 void free_arg(char* arg[]){
@@ -502,5 +569,84 @@ void free_lst(NodeList* head){
 void skip_spaces_tabs(const char* str, int *index){
     while(str[*index] == ' ' || str[*index] == '\t'){
         (*index)++;
+    }
+}
+
+char* arg_into_str(char* args[]){
+    if(args[0] == NULL){
+        return NULL;
+    }
+    int res_ind = 0;
+    static char res[INPUT_SIZE];
+    for(int j = 0; args[0][j] != '\0'; j++){
+        res[res_ind++] = args[0][j];
+    }
+    for(int i = 1; args[i] != NULL; i++){
+        res[res_ind++] = ' ';
+        for(int j = 0; args[i][j] != '\0'; j++){
+            res[res_ind++] = args[i][j];
+        }
+    }
+    res[res_ind] = '\0';
+    return res;
+}
+
+void  delete_job(pid_t pid){
+    if(jobs == NULL){
+        return;
+    }
+    if(jobs->pid == pid){
+        printf("\n[%d] \t Done \t\t %s\n", jobs->count, jobs->command);
+        Jobs* temp = jobs->next;
+        free(jobs);
+        jobs = temp;
+        if(jobs == NULL){
+            job_count = 1;
+        }
+        return;
+    }
+    Jobs* prev = jobs;
+    Jobs* cur = jobs->next;
+    while(cur != NULL){
+        if(cur->pid == pid){
+            printf("\n[%d] \t Done \t\t %s\n", jobs->count, jobs->command);
+            if(cur->next == NULL){
+                job_count = prev->count + 1;
+            }
+            Jobs * temp = cur->next;
+            free(cur);
+            cur = NULL;
+            prev->next = temp;
+            return;
+        }
+        cur = cur->next;
+        prev = prev->next;
+    }
+}
+
+void add_job(pid_t pid, char* command){
+   Jobs *temp = jobs;
+   jobs = (Jobs*) malloc(sizeof(Jobs));
+   jobs->pid = pid;
+   jobs->count = job_count;
+   job_count++;
+   strcpy(jobs->command, command);
+   jobs->next = temp;
+}
+
+void print_jobs(Jobs* jobs_head){
+    if (jobs_head == NULL) {
+        return;
+    }
+    print_jobs(jobs_head->next);
+    printf("[%d] \t Running \t %s &\n", jobs_head->count, jobs_head->command);
+}
+
+void free_jobs() {
+    Jobs * temp;
+    while (jobs != NULL) {
+        temp = jobs;
+        jobs = jobs->next;
+        free(temp);
     }
 }
